c4327107619923d557820e9eb8ce4c2675742b21
Muhammad Alif Budiman <alifm2101@gmail.com>
2025-05-30 10:33:58 +0700

    fix: update paths in configuration to use server.js instead of api/index.js

--- dist/api/index.js
+++ dist/api/index.js
@@ -1,8 +1,3 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-const _server = require("../server");
-_server.app.listen();
 
 //# sourceMappingURL=index.js.map
--- src/server.ts
+++ src/server.ts
@@ -24,3 +24,5 @@ export const app = new App([
   new CommentRoute(),
   new ReplyRoute()
 ]);
+
+app.listen();
