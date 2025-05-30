a1959bec7d0bc5319c9ef2b47b504179afb69604
Muhammad Alif Budiman <alifm2101@gmail.com>
2025-05-30 14:01:14 +0700

    fix: enhance logger setup to support Vercel environment and refactor transport configuration

--- dist/utils/logger.js
+++ dist/utils/logger.js
@@ -27,39 +27,42 @@ function _interop_require_default(obj) {
     };
 }
 const logDir = (0, _path.join)(__dirname, _index.LOG_DIR);
-if (!(0, _fs.existsSync)(logDir)) {
+const isVercel = !!process.env.VERCEL;
+if (!isVercel && !(0, _fs.existsSync)(logDir)) {
     (0, _fs.mkdirSync)(logDir);
 }
 const logFormat = _winston.default.format.printf(({ timestamp, level, message })=>`${timestamp} ${level}: ${message}`);
+const transports = [
+    new _winston.default.transports.Console({
+        format: _winston.default.format.combine(_winston.default.format.splat(), _winston.default.format.colorize())
+    })
+];
+if (!isVercel) {
+    transports.push(new _winstondailyrotatefile.default({
+        level: "debug",
+        datePattern: "YYYY-MM-DD",
+        dirname: logDir + "/debug",
+        filename: "%DATE%.log",
+        maxFiles: 30,
+        json: false,
+        zippedArchive: true
+    }), new _winstondailyrotatefile.default({
+        level: "error",
+        datePattern: "YYYY-MM-DD",
+        dirname: logDir + "/error",
+        filename: "%DATE%.log",
+        maxFiles: 30,
+        handleExceptions: true,
+        json: false,
+        zippedArchive: true
+    }));
+}
 const logger = _winston.default.createLogger({
     format: _winston.default.format.combine(_winston.default.format.timestamp({
         format: "YYYY-MM-DD HH:mm:ss"
     }), logFormat),
-    transports: [
-        new _winstondailyrotatefile.default({
-            level: "debug",
-            datePattern: "YYYY-MM-DD",
-            dirname: logDir + "/debug",
-            filename: "%DATE%.log",
-            maxFiles: 30,
-            json: false,
-            zippedArchive: true
-        }),
-        new _winstondailyrotatefile.default({
-            level: "error",
-            datePattern: "YYYY-MM-DD",
-            dirname: logDir + "/error",
-            filename: "%DATE%.log",
-            maxFiles: 30,
-            handleExceptions: true,
-            json: false,
-            zippedArchive: true
-        })
-    ]
+    transports
 });
-logger.add(new _winston.default.transports.Console({
-    format: _winston.default.format.combine(_winston.default.format.splat(), _winston.default.format.colorize())
-}));
 const stream = {
     write: (message)=>{
         logger.info(message.substring(0, message.lastIndexOf("\n")));
