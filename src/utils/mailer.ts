acf876b3d4852fc54eb2deeb298722c37bf1a44e
Muhammad Alif Budiman <alifm2101@gmail.com>
2025-06-02 10:39:03 +0700

    refactor: update email template path resolution for Vercel compatibility

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -5,8 +5,8 @@ import { sendEmail } from "@config/node-mailer";
 import { logger } from "@utils/logger";
 
 export const sendEmailOTP = async (data): Promise<void> => {
-  const templatePath = path.join(__dirname, "../public/templates/email-verification.ejs");
-  logger.info(`[sendEmailOTP] __dirname: ${__dirname}`);
+  // Use require.resolve to force Vercel to bundle the template
+  const templatePath = require.resolve("../../public/templates/email-verification.ejs");
   logger.info(`[sendEmailOTP] templatePath: ${templatePath}`);
   logger.info(`[sendEmailOTP] file exists: ${fs.existsSync(templatePath)}`);
 
