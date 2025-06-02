070be50724a35cea0f79f51ebb1d8abf0a05ecd2
Muhammad Alif Budiman <alifm2101@gmail.com>
2025-06-02 09:34:36 +0700

    feat: add logging for sendEmailOTP function to track directory usage

--- dist/utils/mailer.js
+++ dist/utils/mailer.js
@@ -11,12 +11,14 @@ Object.defineProperty(exports, "sendEmailOTP", {
 const _ejs = /*#__PURE__*/ _interop_require_default(require("ejs"));
 const _path = /*#__PURE__*/ _interop_require_default(require("path"));
 const _nodemailer = require("../config/node-mailer");
+const _logger = require("./logger");
 function _interop_require_default(obj) {
     return obj && obj.__esModule ? obj : {
         default: obj
     };
 }
 const sendEmailOTP = async (data)=>{
+    _logger.logger.info(`[sendEmailOTP] __dirname: ${__dirname}`);
     const template = await _ejs.default.renderFile(_path.default.join(__dirname, "../public/templates/email-verification.ejs"), {
         data
     });
--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,8 +1,10 @@
 import ejs from "ejs";
 import path from "path";
 import { sendEmail } from "@config/node-mailer";
+import { logger } from "@utils/logger";
 
 export const sendEmailOTP = async (data): Promise<void> => {
+  logger.info(`[sendEmailOTP] __dirname: ${__dirname}`); // log the current directory using logger
   const template = await ejs.renderFile(
     path.join(__dirname, "../public/templates/email-verification.ejs"),
     { data },
